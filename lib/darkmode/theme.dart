7f7a4372fb338e59184fea7b64f4c8691eaa3f71
MOHAMMED NIHAL N <69244384+mohdnihaln@users.noreply.github.com>
2025-03-12 12:39:58 +0530

    Update theme.dart

--- lib/darkmode/theme.dart
+++ lib/darkmode/theme.dart
@@ -2,18 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ThemeCubit extends Cubit<ThemeData> {
-
-  
   ThemeCubit() : super(_lightTheme);
 
   static final _lightTheme = ThemeData(
     brightness: Brightness.light,
     primarySwatch: Colors.blue,
+    // useMaterial3: false, for older version
+    // appBarTheme: AppBarTheme(
+    //   backgroundColor: Colors.blue,
+    //   titleTextStyle: TextStyle(
+    //     color: Colors.white, // Title text color
+    //     fontSize: 25,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    // ),
   );
 
   static final _darkTheme = ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.blue,
+    useMaterial3: false,
+    // appBarTheme: AppBarTheme(
+    //   backgroundColor: Colors.white.withOpacity(0.2),
+    //   titleTextStyle: TextStyle(
+    //     color: Colors.blue, // Title text color
+    //     fontSize: 25,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    // ),
   );
 
   void toggleTheme(bool isDark) {
